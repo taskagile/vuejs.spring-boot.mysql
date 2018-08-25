c8173299b17043bac77303d54c4626a8da4bf45a
James J. Ye <james.ye@hotmail.com>
2018-08-25 15:06:06 +0800

    create back-end scaffold

---
+++ src/main/java/com/taskagile/TaskAgileApplication.java
@@ -0,0 +1,12 @@
+package com.taskagile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskAgileApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TaskAgileApplication.class, args);
+  }
+}
