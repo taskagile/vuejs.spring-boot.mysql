fec3bc822edbc99600e09cc0b6c3fb71bd1547fe
James J. Ye <james.ye@hotmail.com>
2018-08-26 17:53:24 +0800

    fix: invalid return value of encrypt() method

--- src/main/java/com/taskagile/domain/common/security/PasswordEncryptorDelegator.java
+++ src/main/java/com/taskagile/domain/common/security/PasswordEncryptorDelegator.java
@@ -8,6 +8,6 @@ public class PasswordEncryptorDelegator implements PasswordEncryptor {
   @Override
   public String encrypt(String rawPassword) {
     // TODO implement this
-    return null;
+    return rawPassword;
   }
 }
