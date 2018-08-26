ee034f48e190f7f5fcaa1ab381de8be3c13e86d9
James J. Ye <james.ye@hotmail.com>
2018-08-26 17:47:16 +0800

    fix: add /register to request mapping

--- src/main/java/com/taskagile/web/pages/MainController.java
+++ src/main/java/com/taskagile/web/pages/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-  @GetMapping(value = { "/", "/login" })
+  @GetMapping(value = { "/", "/login", "/register" })
   public String entry() {
     return "index";
   }
