5fc352db8d764fedc9ab42b10b7ec74c498c722d
James J. Ye <james.ye@hotmail.com>
2018-08-27 03:37:22 +0800

    fix: add missing test of services.registration.spec.js

--- front-end/tests/unit/services.registration.spec.js
+++ front-end/tests/unit/services.registration.spec.js
@@ -39,4 +39,17 @@ describe('services/registration', () => {
       expect(error.response.message).toEqual('Bad request')
     })
   })
+
+  it('should call `/registrations` API', () => {
+    expect.assertions(1)
+    moxios.wait(() => {
+      let request = moxios.requests.mostRecent()
+      expect(request.url).toEqual('/registrations')
+      request.respondWith({
+        status: 200,
+        response: {result: 'success'}
+      })
+    })
+    return registrationService.register()
+  })
 })
