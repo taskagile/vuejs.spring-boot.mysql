4a6bd324e5bb75018cb3d04480c59b645bcbd786
James J. Ye <james.ye@hotmail.com>
2018-09-29 18:45:33 +0800

    fix: add mapping for card URL

--- src/main/java/com/taskagile/web/pages/MainController.java
+++ src/main/java/com/taskagile/web/pages/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-  @GetMapping(value = { "/", "/login", "/register", "/board/*" })
+  @GetMapping(value = { "/", "/login", "/register", "/board/*", "/card/**" })
   public String entry() {
     return "index";
   }
