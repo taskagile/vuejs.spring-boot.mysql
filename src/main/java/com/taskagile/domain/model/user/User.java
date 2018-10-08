97b9c1cde891c752fff9b96dd3c127678584de3f
James J. Ye <james.ye@hotmail.com>
2018-10-09 01:42:38 +0800

    fix: increase password field length

--- src/main/java/com/taskagile/domain/model/user/User.java
+++ src/main/java/com/taskagile/domain/model/user/User.java
@@ -22,7 +22,7 @@ public class User extends AbstractBaseEntity {
   @Column(name = "email_address", nullable = false, length = 100, unique = true)
   private String emailAddress;
 
-  @Column(name = "password", nullable = false, length = 30)
+  @Column(name = "password", nullable = false, length = 128)
   private String password;
 
   @Column(name = "first_name", nullable = false, length = 45)
